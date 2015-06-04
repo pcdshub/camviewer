77a63d9ca206f6f3b29ca24012dbc341b983c3a7
Michael Browne <mcbrowne@slac.stanford.edu>
2015-06-04 17:38:08 +0000

    Update to new-style EVR screens.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pycaqt/camviewer@21598 ececa60f-9f63-0410-97c7-ea05888eb260

--- openEvr.sh
+++ openEvr.sh
@@ -8,10 +8,12 @@ unset EDMLIBS
 unset EDMFILES
 unset EPICS_BASE
 source /reg/g/pcds/package/epics/3.14/tools/current/bin/epicsenv.sh
-cd /reg/g/pcds/package/epics/3.14/modules/event/R4.0.0-0.4.0/
-if [ `caget -t $1:CTRL.FPGV` -ge 7936 ]; then
-    file=evrScreens/evr_slac.edl
+cd /reg/g/pcds/package/epics/3.14/modules/event/R4.0.0-2.9.0/
+if [ `caget -t $1:CTRL.FPGV` -lt 520093696 ]; then
+#    file=evrScreens/evr.edl
+     file=evrScreens/evrPmc230.edl
 else
-    file=evrScreens/evr.edl
+#    file=evrScreens/evr_slac.edl
+     file=evrScreens/evrSLAC.edl
 fi
 edm -eolc -x -m "EVR=$1" $file >/dev/null 2>/dev/null
