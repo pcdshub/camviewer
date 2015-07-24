be1f3908f9c35dc6e320ecb170794aa45b435030
Bruce Hill <bhill@slac.stanford.edu>
2015-07-24 00:03:50 +0000

    Added COMPILE.sh and removed ui python files created by pyuic4 Also added an svn:ignore property for cleaner svn status
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/pycaqt/camviewer@21943 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ COMPILE.sh
@@ -0,0 +1,10 @@
+export PSPKG_RELEASE=controls-0.0.6
+source $PSPKG_ROOT/etc/set_env.sh
+make
+pyuic4 -o camviewer_ui.py camviewer.ui
+pyuic4 -o advanced_ui.py advanced.ui
+pyuic4 -o markers_ui.py markers.ui
+pyuic4 -o specific_ui.py specific.ui
+pyuic4 -o droplet_ui.py droplet.ui
+pyuic4 -o xtcrdr_ui.py xtcrdr.ui
+pyuic4 -o timeout_ui.py timeout.ui
