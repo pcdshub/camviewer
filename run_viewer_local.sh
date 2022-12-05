abca72c2107c18a6a9dec502857b13edd168d713
Michael Browne <mcbrowne@slac.stanford.edu>
2022-12-05 09:56:54 -0800

    Fix up source in .sh files.  Update open_evr.sh to use newer files and methods.

--- run_viewer_local.sh
+++ run_viewer_local.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -f
 
 export PCDS_CONDA_VER=5.1.1
-source /cds/group/pcds/pyps/conda/pcds_conda
+source /cds/group/pcds/pyps/conda/pcds_conda ""
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
 ulimit -c unlimited
 
 echo Launching camviewer w/ $@ from `pwd`
 ./camviewer.pyw $@
-
